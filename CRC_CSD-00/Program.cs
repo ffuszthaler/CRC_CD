1fbcdb62ae96da356bdbd40a65a1471e6ae4636a
ffuszthaler <flo.fuszthaler@gmail.com>
2023-03-07 08:35:09 +0100

    added previous exercises

---
+++ CRC_CSD-00/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSD_01_01
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hi there!");
+      Console.WriteLine("The length of the arguments: " + args.Length);
+
+      // print out parameters
+      for (int i = 0; i < args.Length; i++)
+      {
+        Console.WriteLine(args[i]);
+      }
+    }
+  }
+}
---
+++ CRC_CSD-01/Program.cs
@@ -0,0 +1,42 @@
+﻿// FLOAT
+Console.WriteLine("Using float:");
+float a = 0.1f;
+float b = 0.2f;
+
+if (a + b == 0.3f)
+{
+  Console.WriteLine($"{a} + {b} equals 0.3");
+}
+else
+{
+  Console.WriteLine($"{a} + {b} does NOT equals 0.3");
+}
+
+// DOUBLE
+Console.WriteLine("Using doubles:");
+double c = 0.1;
+double d = 0.2;
+
+if (c + d == 0.3)
+{
+  Console.WriteLine($"{c} + {d} equals 0.3");
+}
+else
+
+{
+  Console.WriteLine($"{a} + {d} does NOT equals 0.3");
+}
+
+// DECIMAL
+Console.WriteLine("Using decimals:");
+decimal e = 0.1M;
+decimal f = 0.2M;
+
+if (e + f == 0.3M)
+{
+  Console.WriteLine($"{e} + {f} equals 0.3");
+}
+else
+{
+  Console.WriteLine($"{e} + {f} does NOT equals 0.3");
+}
